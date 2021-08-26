4f8ab609ac094db32c002783f7fdeff079be0e14
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-08-26 11:28:48 +0200

    :rocket: My First App in Vue and reinforcement JS

---
+++ 02-bases-vue/app.js
@@ -0,0 +1,9 @@
+
+const app = Vue.createApp({
+    template: `
+        <h1>Hola Mundo</h1>
+        <p>Desde app.js</p>
+    `
+})
+
+app.mount('#myApp')
---
+++ README.md
@@ -0,0 +1,2 @@
+# Code Of Course 'Vue.js: De cero a Experto' from Udemy
+## 👨‍💻 In this moment I'm working with my first app in Vue 🚀
