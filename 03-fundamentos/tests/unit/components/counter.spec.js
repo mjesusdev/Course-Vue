d1acf8d264563aaba259588ca880a298a2e132e8
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-09-13 11:34:23 +0200

    Finish the Counter Test, events in Tests :rocket:

--- 03-fundamentos/tests/unit/components/counter.spec.js
+++ 03-fundamentos/tests/unit/components/counter.spec.js
@@ -3,14 +3,18 @@ import { shallowMount, mount } from '@vue/test-utils'
 
 describe('Counter Component', () => {
 
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallowMount(Counter)
+    })
+    
     /* test('should match with Snapshot', () => {
         const wrapper = shallowMount( Counter )
         expect( wrapper.html() ).toMatchSnapshot()
     }) */
 
     test('h2 should have the value by default "Counter"', () => {
-        const wrapper = shallowMount( Counter )
-    
         /* expect( wrapper.find('h10').exists() ).toBeTruthy() */
 
         const h2Value = wrapper.find('h2').text()
@@ -19,14 +23,45 @@ describe('Counter Component', () => {
     })
 
     test('the value by default should be 100 on the <p>', () => {
-        // 1.Step - Mount the component with wrapper
-        const wrapper = shallowMount( Counter )
-
         // 2.Step - Find the <p> with attribute "data-testid"
         const value = wrapper.find('[data-testid="counter"]').text()
 
         // 3.Step - Assert the value
         expect( value ).toBe( '100' )
+    })
+
+    test('the value should be incremented and decrement the counter', async() => {
+        const [ increaseBtn, decreaseBtn ] = wrapper.findAll('button')
+        
+        // Simulate the click event
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+        
+        const value = wrapper.find('[data-testid="counter"]').text()
+        
+        expect( value ).toBe('101');
+    })
+
+    test('the value should be set up by default', () => {
+        const { start } = wrapper.props()
+
+        const value = wrapper.find('[data-testid="counter"]').text()
+
+        expect( Number(value) ).toBe( start )
+    })
+
+    test('the prop title should be show', () => {
+        const wrapper = shallowMount( Counter, {
+            props: {
+                title: 'Hola Mundo'
+            }
+        })
+
+        expect( wrapper.find('h2').text() ).toBe(title);
     })
 
 })
