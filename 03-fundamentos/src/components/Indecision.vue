81e176cc4e3573f10fe6dec76ae8049700aebc27
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-09-14 12:11:04 +0200

    Finish tests in 03-fundamentos :green_heart:

--- 03-fundamentos/src/components/Indecision.vue
+++ 03-fundamentos/src/components/Indecision.vue
@@ -43,6 +43,8 @@ export default {
 
         this.isValidQuestion = false
 
+        console.log({ value });
+
         if ( !value.includes('?') ) return
 
         this.isValidQuestion = true
--- 03-fundamentos/tests/unit/components/indecision.spec.js
+++ 03-fundamentos/tests/unit/components/indecision.spec.js
@@ -1,16 +1,70 @@
+import { shallowMount } from '@vue/test-utils'
 import Indecision from '@/components/Indecision';
-import { shallowMount, mount } from '@vue/test-utils'
 
 describe('Component Indecision', () => {
 
-    let wrapper;
+    let wrapper
+    let clgSpy
+
+    global.fetch = jest.fn( () => Promise.resolve({
+        json: () => Promise.resolve({
+            answer: 'yes',
+            forced: false,
+            image: 'https://yesno.wtf/assets/yes/2.gif'
+        })
+    }) )
 
     beforeEach(() => {
         wrapper = shallowMount( Indecision )
+
+        clgSpy = jest.spyOn(console, 'log')
+
+        jest.clearAllMocks()
     })
 
-    test('should do match with the snapshot', () => {
-        expect( wrapper.html() ).toMatchSnapshot();
+    test('Should do match with the snapshot', () => {
+        expect( wrapper.html() ).toMatchSnapshot()
+    })
+
+    test('Write in the input not should nothing', async() => {
+        const getAnswerSpy = jest.spyOn( wrapper.vm, 'getAnswer' )
+
+        const input = wrapper.find('input')
+        await input.setValue('Hello World!')
+
+        expect( clgSpy ).toHaveBeenCalledTimes(1)
+        expect( getAnswerSpy ).not.toHaveBeenCalled() 
+    })
+    
+    test('Write "?" should return the getAnswer', async() => {
+        const getAnswerSpy = jest.spyOn( wrapper.vm, 'getAnswer' )
+    
+        const input = wrapper.find('input')
+        await input.setValue('Hello World?')
+    
+        expect( clgSpy ).toHaveBeenCalledTimes(1)
+        expect( getAnswerSpy ).toHaveBeenCalled()
+    })
+
+    test('Test in getAnswer', async() => {
+        await wrapper.vm.getAnswer()
+
+        const img = wrapper.find('img')
+
+        expect( img.exists() ).toBeTruthy()
+        expect( wrapper.vm.img ).toBe('https://yesno.wtf/assets/yes/2.gif')
+        expect( wrapper.vm.answer ).toBe('Sí!')
+    })
+
+    test('Test in getAnswer - Error in API', async() => {
+        fetch.mockImplementationOnce( () => Promise.reject('API is down') )
+
+        await wrapper.vm.getAnswer()
+
+        const img = wrapper.find('img')
+
+        expect( img.exists() ).toBeFalsy()
+        expect( wrapper.vm.answer ).toBe('The API cannot start')
     })
 
 })
