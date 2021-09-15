0af5c2d517e2213b04c83a0fcdbcb1f2184e786a
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-09-15 11:58:40 +0200

    Start new project | Pokemon Game :rocket:

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>¿Quién es este pokémon?</h1>
+
+      <PokemonPicture :pokemonId="4" :showPokemon="false"/>
+
+      <PokemonOptions />
+
+  </div>
+</template>
+
+<script>
+    import PokemonOptions from '@/components/PokemonOptions'
+    import PokemonPicture from '@/components/PokemonPicture'
+
+    import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+    console.log( getPokemonOptions() );
+
+    export default {
+        components: { PokemonPicture, PokemonOptions },
+    }
+</script>
