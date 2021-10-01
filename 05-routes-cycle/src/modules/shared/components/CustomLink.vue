d9d61c03bc49c11ad1b6652ea06d317ab2d86f88
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-10-01 10:21:03 +0200

    Create a CustomLink in Vue :green_heart:

---
+++ 05-routes-cycle/src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,46 @@
+<template>
+    <li class="nav-item">
+        <a v-if="isExternalLink" 
+            target="_blank"
+            :href="link.to" class="nav-link normal-link">{{ link.name }}
+        </a>
+
+        <router-link
+            v-else
+            :to="link.to"
+            class="nav-link"
+            v-slot="{ href, isActive }"
+        >
+            <a :href='href' 
+                :class=" isActive ? 'is-active' : 'normal-link' ">
+                {{ link.name }}
+            </a>
+        </router-link>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: {
+            link: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            isExternalLink() {
+                return this.link.to.startsWith('https');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .is-active {
+        color: #42b983 !important;
+    }
+
+    .normal-link {
+        color: #c6c5c5;
+    }
+</style>
--- 05-routes-cycle/src/modules/shared/components/Navbar.vue
+++ 05-routes-cycle/src/modules/shared/components/Navbar.vue
@@ -1,34 +1,45 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'home' }">
-                            Home
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'pokemon-id', params: { id: 100 } }">
-                            Pokemon by id
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'about' }">
-                            About
-                        </router-link>
-                    </li>
+                    <CustomLink
+                        v-for="link in links"
+                        :key="link.to"
+                        :link="link"
+                    />
                 </ul>
             </div>
         </div>
     </nav>
 </template>
 
-<style scoped>
-    a.router-link-exact-active {
-        color: #42b983 !important;
+<script>
+
+    import { defineAsyncComponent } from 'vue'
+
+    export default {
+
+        data() {
+            return {
+                links: [
+                    { to: '/home', name: 'Pokemons' },
+                    { to: '/pokemonid/50', name: 'By Id'},
+                    { to: '/about', name: 'About' },
+                    { to: 'https://duckduckgo.com', name: 'DuckDuckGo'}
+                ]
+            }
+        },
+
+        components: {
+            CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+        }
     }
+</script>
+
+<style>
 </style>
