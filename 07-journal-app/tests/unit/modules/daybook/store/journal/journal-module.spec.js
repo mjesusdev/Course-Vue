f43f97c9a92834d0a5f40ad8c5ca14a038557a1e
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-11-10 10:55:26 +0100

    Tests of mutations - getters, updateEntry, etc :rocket:

--- 07-journal-app/tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ 07-journal-app/tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -14,15 +14,85 @@ const createVuexStore = ( initialState ) =>
     })
 
 describe('Vuex - Tests in Journal Module', () => {
-    
     test('this is initial state, should have this store', () => {
-        
         const store = createVuexStore(journalState)
         const { isLoading, entries } = store.state.journal
 
         expect( isLoading ).toBeFalsy()
         expect( entries ).toEqual( journalState.entries )
+    })
 
+    // Mutations
+    test('mutation: setEntries', () => {
+        const store = createVuexStore({ isLoading: true, entries: [] })
+        
+        store.commit('journal/setEntries', journalState.entries)
+        expect( store.state.journal.entries.length ).toBe(3)
+        
+        store.commit('journal/setEntries', journalState.entries)
+        expect( store.state.journal.entries.length ).toBe(6)
+        
+        expect( store.state.journal.isLoading ).toBeFalsy()
+    })
+    
+    test('mutation: updateEntry', () => {
+        // 1. Create store
+        const store = createVuexStore( journalState )
+
+        // 2. UpdatedEntry
+        const updatedEntry = {
+            id: '-Mn5HXnHBBwU8rtrMPZ8',
+            date : 1635414309496,
+            text : 'Hello World from tests'
+        }
+
+        // 3. Commit mutation
+        store.commit('journal/updateEntry', updatedEntry)
+
+        // 4. Expects entries.length = 2
+        const storeEntries = store.state.journal.entries
+        expect( storeEntries.length ).toBe(3)
+
+        // entries toEqual UpdatedEntry
+        expect( storeEntries.find( e => e.id === updatedEntry.id ) ).toEqual( updatedEntry )
+    })
+
+    test('mutation: addEntry & deleteEntry', () => {
+        // 1. Create store
+        const store = createVuexStore( journalState )
+
+        // 2. Create new entry
+        const newEntry = {
+            id: 'ABC-123',
+            text: 'Hello World'
+        }
+
+        // 3. Commit mutation
+        store.commit('journal/addEntry', newEntry)
+
+        // 4. Expects entries.length = 4
+        const storeEntries = store.state.journal.entries
+        expect( storeEntries.length ).toBe(4)
+        expect( storeEntries.find( e => e.id === newEntry.id ) ).toBeTruthy()
+
+        // 5. Commit mutation
+        store.commit('journal/deleteEntry', newEntry.id)
+        expect( store.state.journal.entries.length ).toBe(3)
+        expect( store.state.journal.entries.find( e => e.id === newEntry.id ) ).toBeFalsy()
+    })
+
+    test('getters: getEntriesByTerm & getEntryById', () => {
+        const store = createVuexStore( journalState )
+
+        const [ entry1, entry2, entry3 ] = journalState.entries
+
+        expect( store.getters['journal/getEntriesByTerm']('').length ).toBe(3)
+        expect( store.getters['journal/getEntriesByTerm']('ni modo 😅').length ).toBe(1)
+
+        expect( store.getters['journal/getEntriesByTerm']('ni modo 😅') ).toEqual([ entry2 ])
+
+        // getEntryById
+        expect( store.getters['journal/getEntryById']('-Mn5HXnHBBwU8rtrMPZ8') ).toEqual( entry1 )
     })
 
 })
