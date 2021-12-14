121f5d4ca2b93938a6a91f55e0238efaae6e5ec0
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-12-14 11:27:18 +0100

    Continue with section 08-compo-api :green_heart:

--- 08-compo-api/src/App.vue
+++ 08-compo-api/src/App.vue
@@ -4,7 +4,11 @@
     <router-link to="/about">About</router-link> |
     <router-link :to="{ name: 'counter' }">Counter</router-link>
   </div>
-  <router-view/>
+  <router-view v-slot="{ Component, route }">
+    <keep-alive>
+        <component :is="Component" :key="route.name" />
+    </keep-alive>
+  </router-view>
 </template>
 
 <style>
--- 08-compo-api/src/views/About.vue
+++ 08-compo-api/src/views/About.vue
@@ -1,5 +1,82 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle">Change Title</button>
   </div>
 </template>
+
+<script>
+
+  import {
+    ref,
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted,
+    onErrorCaptured,
+    onRenderTracked,
+    onRenderTriggered,
+    onActivated,
+    onDeactivated,
+  } from 'vue'
+
+  export default {
+    setup() {
+        const title = ref('About')
+
+        console.log('setup calls')
+
+        onBeforeMount(() => {
+            console.log('onBeforeMount');
+        })
+
+        onMounted(() => {
+            console.log('onMounted');
+        })
+
+        onBeforeUpdate(() => {
+            console.log('onBeforeUpdate');
+        })
+
+        onUpdated(() => {
+            console.log('onUpdated');
+        })
+
+        onBeforeUnmount(() => {
+            console.log('onBeforeUnmount');
+        })
+
+        onUnmounted(() => {
+            console.log('onUnmounted');
+        })
+
+        onErrorCaptured(() => {
+            console.log('onErrorCaptured');
+        })
+
+        onRenderTracked(() => {
+            console.log('onRenderTracked');
+        })
+
+        onRenderTriggered(() => {
+            console.log('onRenderTriggered');
+        })
+
+        onActivated(() => {
+            console.log('onActivated');
+        })
+
+        onDeactivated(() => {
+            console.log('onDeactivated');
+        })
+
+        return {
+          title,
+          changeTitle: () => title.value = 'New About'
+        }
+    },
+  }
+</script>
+
