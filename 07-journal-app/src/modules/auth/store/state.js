de37082ad73f53cde6f4d6523af5a246f5e990d7
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-01-26 11:42:42 +0100

    Create store of auth :rocket:

---
+++ 07-journal-app/src/modules/auth/store/state.js
@@ -0,0 +1,6 @@
+export default () => ({
+    status: 'authenticating', // 'authenticated', 'not-authenticated', 'authenticating'
+    user: null,
+    idToken: null,
+    refreshToken: null
+})
--- 07-journal-app/src/store/index.js
+++ 07-journal-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 
+import auth from '../modules/auth/store'
 import journal from '../modules/daybook/store/journal'
 
 const store = createStore({
     modules: {
+        auth,
         journal
     }
 })
