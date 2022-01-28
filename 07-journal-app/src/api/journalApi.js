499217fb1b85f8e518f70abf39b2e1a310373f85
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-01-28 12:36:15 +0100

    Add interceptors of Axios | Final Section, continue with tests :rocket:

--- 07-journal-app/src/api/journalApi.js
+++ 07-journal-app/src/api/journalApi.js
@@ -4,4 +4,12 @@ const journalApi = axios.create({
     'baseURL': 'https://vue-demos-26ce5-default-rtdb.europe-west1.firebasedatabase.app'
 })
 
+journalApi.interceptors.request.use( (config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    return config
+})
+
 export default journalApi
