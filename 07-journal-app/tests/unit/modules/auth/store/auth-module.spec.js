76bbc4b8a4e4f365f3fee5b468baa9274edd4b8a
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-02-08 12:33:56 +0100

    Create new tests, working with Actions, User, etc

--- 07-journal-app/tests/unit/modules/auth/store/auth-module.spec.js
+++ 07-journal-app/tests/unit/modules/auth/store/auth-module.spec.js
@@ -1,6 +1,9 @@
+import axios from 'axios'
 import createVuexStore from '../../../mock-data/mock-store'
 
 describe('Vuex: Tests in authModule', () => {
+    const apiKey = process.env.VUE_APP_APIKEY
+
     test('initial state', () => {
         const store = createVuexStore({
             status: 'authenticating',
@@ -64,5 +67,69 @@ describe('Vuex: Tests in authModule', () => {
 
         expect(localStorage.getItem('idToken')).toBeFalsy()
         expect(localStorage.getItem('refreshToken')).toBeFalsy()
+    })
+
+    test('Getter: username currentState', () => {
+        const store = createVuexStore({
+            status: 'authenticated',
+            user: { name: 'Pepe', email: 'pepe@gmail.com' },
+            idToken: 'ABC-123',
+            refreshToken: 'XYZ-456'
+        })
+
+        expect( store.getters['auth/currentState'] ).toBe('authenticated')
+        expect( store.getters['auth/username'] ).toBe('Pepe')
+    })
+
+    test('Actions: createUser - Error user exists yet', async() => {
+        const store = createVuexStore({
+            status: 'not-authenticated',
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+       const newUser = { name: 'Test User', email: 'test@test.com', password: '123456' } 
+       
+       const resp = await store.dispatch('auth/createUser', newUser)
+       expect( resp ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+       
+       const { status, user, idToken, refreshToken } = store.state.auth
+       
+       expect(status).toBe('not-authenticated')
+       expect(user).toBeFalsy()
+       expect(idToken).toBeFalsy()
+       expect(refreshToken).toBeFalsy()
+    })
+    
+    test('Actions: createUser signInUser - Crea el usuario', async() => {
+        const store = createVuexStore({
+            status: 'not-authenticated',
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+    
+       const newUser = { name: 'Test User', email: 'test2@test.com', password: '123456' }
+
+       // Action: SignIn
+       await store.dispatch('auth/signInUser', newUser)
+       const { idToken } = store.state.auth
+
+       // Delete user
+       const deleteResp = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:delete?key= ' + apiKey, {
+           idToken
+       })
+
+       // Create user
+       const resp = await store.dispatch('auth/createUser', newUser)
+       expect(resp).toEqual({ ok:true })
+
+       const { status, user, idToken: token, refreshToken } = store.state.auth
+       
+       expect(status).toBe('authenticated')
+       expect(user).toMatchObject({ name: 'Test User', email: 'test2@test.com' })
+       expect(typeof token).toBe('string')
+       expect(typeof refreshToken).toBe('string')
     })
 })
