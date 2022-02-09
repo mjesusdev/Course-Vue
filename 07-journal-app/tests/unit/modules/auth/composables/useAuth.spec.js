e9e18982869bc2dface415689f457d07385317dc
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-02-09 11:03:45 +0100

    CreateUser second test - Email Exists

--- 07-journal-app/tests/unit/modules/auth/composables/useAuth.spec.js
+++ 07-journal-app/tests/unit/modules/auth/composables/useAuth.spec.js
@@ -20,8 +20,18 @@ describe('Tests in useAuth', () => {
         mockStore.dispatch.mockReturnValue({ ok: true })
 
         const resp = await createUser( newUser )    
-        expect( mockStore.dispatch ).toHaveBeenCalledWith("auth/createUser", {"email": "pepe@gmail.com", "name": "Pepe"})
-        expect( resp ).toBe({ ok: true })
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/createUser', newUser)
+        expect( resp ).toEqual({ ok: true })
     })
-      
+
+    test('createUser failed, because the user exists yet', async() => {
+        const { createUser } = useAuth()
+
+        const newUser = { name: 'Pepe', email: 'pepe@gmail.com' }
+        mockStore.dispatch.mockReturnValue({ ok: false, message: 'EMAIL_EXISTS' })
+
+        const resp = await createUser( newUser )  
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/createUser', newUser)
+        expect( resp ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+    })
 })
