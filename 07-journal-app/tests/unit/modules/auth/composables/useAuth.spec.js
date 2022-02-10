2976a860265a096340ed017dd76a54822a9f8717
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-02-10 11:02:20 +0100

    Finish tests in useAuth :rocket:

--- 07-journal-app/tests/unit/modules/auth/composables/useAuth.spec.js
+++ 07-journal-app/tests/unit/modules/auth/composables/useAuth.spec.js
@@ -2,7 +2,11 @@ import useAuth from '@/modules/auth/composables/useAuth'
 
 const mockStore = {
     dispatch: jest.fn(),
-    // Commit and Getters
+    commit: jest.fn(),
+    getters: {
+        'auth/currentState': 'authenticated',
+        'auth/username': 'Pepe'
+    }
 }
 
 jest.mock('vuex', () => ({
@@ -33,5 +37,48 @@ describe('Tests in useAuth', () => {
         const resp = await createUser( newUser )  
         expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/createUser', newUser)
         expect( resp ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+    })
+    
+    test('login succesful', async() => {
+        const { loginUser } = useAuth()
+        const loginForm = { email: 'test@test.com', password: '123456' }
+        mockStore.dispatch.mockReturnValue({ ok: true })
+
+        const resp = await loginUser( loginForm )
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/signInUser', loginForm)
+        expect( resp ).toEqual({ ok: true })
+    })
+
+    test('login failed', async() => {
+        const { loginUser } = useAuth()
+        const loginForm = { email: 'test@test.com', password: '123456' }
+        mockStore.dispatch.mockReturnValue({ ok: false, message: 'EMAIL/PASSWORD do not exist' })
+
+        const resp = await loginUser( loginForm )
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/signInUser', loginForm)
+        expect( resp ).toEqual({ ok: false, message: 'EMAIL/PASSWORD do not exist' })
+    })
+
+    test('checkAuthStatus', async() => {
+        const { checkAuthStatus } = useAuth()
+        mockStore.dispatch.mockReturnValue({ ok: true })
+
+        const resp = await checkAuthStatus()
+        expect( mockStore.dispatch ).toHaveBeenCalledWith('auth/checkAuthentication')
+        expect( resp ).toEqual({ ok: true })
+    })
+
+    test('logout', () => {
+        const { logout } = useAuth()
+        logout()
+
+        expect( mockStore.commit ).toHaveReturnedWith('auth/logout')
+        expect( mockStore.commit ).toHaveReturnedWith('journal/clearEntries')
+    })
+
+    test('Computed: authState, username', () => { 
+        const { authStatus, username } = useAuth()
+        expect(authStatus.value).toBe('authenticated')
+        expect(username.value).toBe('Pepe')
     })
 })
